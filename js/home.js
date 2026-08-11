17b4700fc40a8d2f1798246f3697ea4aa26df07c
mihomatsuoka <matsuoka-31fj@g.bears.ed.jp>
2026-08-11 22:24:40 +0900

    Create home.js

---
+++ js/home.js
@@ -0,0 +1,186 @@
+/* ==========================================
+   Project ぽちゃん
+   home.js
+   ホーム画面
+========================================== */
+function drawHome(){
+
+    currentPage = "home";
+
+    app.innerHTML = `
+
+<header class="header">
+
+    <h1>ぽちゃん</h1>
+
+    <p>毎日のお風呂を、少し快適に。</p>
+
+</header>
+
+
+<section class="today">
+
+    <h2>♨ 本日の湯</h2>
+
+    <p>${state.crowded}</p>
+
+</section>
+
+<section class="card">
+
+    <h2>🛁 入浴中</h2>
+
+    <div class="current">
+
+        <div class="person">
+
+            ${state.current.icon}
+            ${state.current.name}
+
+        </div>
+
+        <div class="status">
+
+            ${state.current.status}
+
+        </div>
+
+    </div>
+
+    <p class="timer">
+
+        ⏱ ${getBathTime()}
+
+    </p>
+
+</section>
+
+<section class="card">
+
+    <h2>🪵 待機中</h2>
+
+    <ul class="queue">
+
+        ${drawQueue()}
+
+    </ul>
+
+</section>
+
+<section class="card">
+
+    <h2>📜 本日の記録</h2>
+
+    ${drawHistory()}
+
+</section>　
+
+    ${drawButtons()}
+
+
+    ${drawBottomMenu()}
+
+`;
+
+}
+
+/* ==========================================
+   下メニュー
+========================================== */
+
+function drawBottomMenu(){
+
+    return `
+
+<nav class="bottom-menu">
+
+<button onclick="drawHome()">
+
+<span>🏠</span>
+
+<small>ホーム</small>
+
+</button>
+
+<button onclick="drawStats()">
+
+<span>📊</span>
+
+<small>統計</small>
+
+</button>
+
+<button onclick="drawHistoryPage()">
+
+<span>📜</span>
+
+<small>履歴</small>
+
+</button>
+
+<button onclick="drawSettings()">
+
+<span>⚙️</span>
+
+<small>設定</small>
+
+</button>
+
+</nav>
+
+`;
+
+}
+/* ==========================================
+   ボタン表示
+========================================== */
+
+function drawButtons(){
+
+    // お風呂が空いている
+    if(state.current.status === "空き"){
+
+        return `
+
+<div class="button-group">
+
+    <button
+        class="main-button"
+        onclick="joinQueue()">
+
+        ➕ 順番待ち
+
+    </button>
+
+    <button
+        class="main-button secondary"
+        onclick="startBath()">
+
+        ♨ 入浴開始
+
+    </button>
+
+</div>
+
+`;
+
+    }
+
+    // 入浴中
+    return `
+
+<div class="button-group">
+
+    <button
+        class="main-button danger"
+        onclick="finishBath()">
+
+        🚪 入浴終了
+
+    </button>
+
+</div>
+
+`;
+
+}
