a57f70d34b76968e22f248b8e3c07c27ff8abd9f
mihomatsuoka <matsuoka-31fj@g.bears.ed.jp>
2026-08-23 14:03:55 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,70 @@
-self.addEventListener("install", () => {
+/* ==========================================
+   Project ぽちゃん
+   service-worker.js
+========================================== */
 
-    console.log("ぽちゃん Service Worker インストール");
+
+// ==========================================
+// インストール
+// ==========================================
+
+self.addEventListener("install", event => {
+
+    console.log(
+        "ぽちゃん Service Worker インストール"
+    );
 
     self.skipWaiting();
 
 });
 
 
-self.addEventListener("activate", () => {
+// ==========================================
+// 有効化
+// ==========================================
 
-    console.log("ぽちゃん Service Worker 有効化");
+self.addEventListener("activate", event => {
+
+    console.log(
+        "ぽちゃん Service Worker 有効化"
+    );
+
+    event.waitUntil(
+        self.clients.claim()
+    );
 
 });
+
+
+// ==========================================
+// 通知クリック
+// ==========================================
+
+self.addEventListener(
+    "notificationclick",
+    event => {
+
+        event.notification.close();
+
+        event.waitUntil(
+            self.clients.matchAll({
+                type: "window",
+                includeUncontrolled: true
+            })
+            .then(clients => {
+
+                if(clients.length > 0){
+
+                    return clients[0].focus();
+
+                }
+
+                return self.clients.openWindow(
+                    "./"
+                );
+
+            })
+        );
+
+    }
+);
