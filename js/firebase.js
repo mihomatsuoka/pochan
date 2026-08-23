e660a99b5ca2f9955554f3c4e849aac900024686
mihomatsuoka <matsuoka-31fj@g.bears.ed.jp>
2026-08-23 17:13:31 +0900

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -7,8 +7,17 @@ import {
     onValue
 } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-database.js";
 
+import {
+    getMessaging,
+    getToken,
+    onMessage
+} from "https://www.gstatic.com/firebasejs/12.17.1/firebase-messaging.js";
 
+
+// ==========================================
 // Firebase設定
+// ==========================================
+
 const firebaseConfig = {
 
     apiKey: "AIzaSyDY8gBLVlYP3V4QjVIN7QknguBYi-wApgw",
@@ -28,26 +37,81 @@ const firebaseConfig = {
 };
 
 
-// Firebaseを初期化
-const firebaseApp = initializeApp(firebaseConfig);
+// ==========================================
+// Firebase初期化
+// ==========================================
+
+const firebaseApp =
+    initializeApp(firebaseConfig);
 
 
+// ==========================================
 // Realtime Database
-const db = getDatabase(
-    firebaseApp,
-    "https://potyan-8ff1f-default-rtdb.asia-southeast1.firebasedatabase.app"
-);
+// ==========================================
+
+const db =
+    getDatabase(
+        firebaseApp,
+        "https://potyan-8ff1f-default-rtdb.asia-southeast1.firebasedatabase.app"
+    );
 
 
+// ==========================================
+// Firebase Messaging
+// ==========================================
+
+const messaging =
+    getMessaging(firebaseApp);
+
+
+// ==========================================
+// VAPID公開鍵
+// ==========================================
+
+const vapidKey =
+    "BP-hB_2FY3RkoolT4Cp9ApEmXW8LbNT9Lk5wtNmYCUo6UL3Sittv_8eKgSLI9rLFNQsgkwMwvPMhRlk1wblw_HI";
+
+
+// ==========================================
 // 接続確認
+// ==========================================
+
 console.log("Firebase 接続OK");
 console.log(db);
 
+console.log(
+    "Firebase Messaging 接続OK"
+);
 
+
+// ==========================================
 // 他のJSから使えるようにする
+// ==========================================
+
 window.firebaseDB = db;
+
 window.firebaseRef = ref;
+
 window.firebaseSet = set;
+
 window.firebaseOnValue = onValue;
 
-console.log("firebaseOnValue:", window.firebaseOnValue);
+
+// FCM用
+window.firebaseMessaging = messaging;
+
+window.firebaseGetToken = getToken;
+
+window.firebaseOnMessage = onMessage;
+
+window.firebaseVapidKey = vapidKey;
+
+
+console.log(
+    "firebaseOnValue:",
+    window.firebaseOnValue
+);
+
+console.log(
+    "FCM準備OK"
+);
